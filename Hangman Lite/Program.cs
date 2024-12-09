efb6abde474c14b80a1df3433dabf3b3e315be03
Jack Gallagher <339690067@GoTVDSB.ca>
2024-12-09 09:03:46 -0500

    Add project files.

---
+++ Hangman Lite/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hangman_Lite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
