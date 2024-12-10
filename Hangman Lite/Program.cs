739b710836603d865b3523a2be5ad71b32976324
Jack Gallagher <339690067@GoTVDSB.ca>
2024-12-10 09:39:44 -0500

    Class ended

--- Hangman Lite/Program.cs
+++ Hangman Lite/Program.cs
@@ -4,16 +4,26 @@
     {
         static void Main(string[] args)
         {
-            string secretWord, displayWord, letterGuess;
-            int incorrectGuesses;
+            string secretWord, displayWord, letterGuess, unguessedLetters;
+            int incorrectGuesses, letterIndex;
             bool done;
 
             List<string> guessedLetters = new List<string>();
 
-            secretWord = "COMPUTER";
-            displayWord = "________";
+            List<string> secretWords = new List<string>() {"COMPUTER", "TABLE", "TRIANGLE", "SQUARE", "TREE", "MOON", "LENGTH"};
+
+            Random generator = new Random();
+
             incorrectGuesses = 0;
             done = false;
+
+            secretWord = secretWords[generator.Next(secretWords.Count)];
+            unguessedLetters = secretWord;
+            displayWord = "";
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                displayWord = displayWord + "_";
+            }
 
             while (!done)
             {
@@ -40,7 +50,7 @@
 
                 Console.WriteLine();
 
-                Console.Write("You have " + (3 - incorrectGuesses) + " guesses left! The letters you have already guessed are: ");
+                Console.Write("You have " + (3 - incorrectGuesses) + " incorrect guesses left! The letters you have already guessed are: ");
                 foreach (string letter in guessedLetters)
                 {
                     Console.Write(letter + ", ");
@@ -52,22 +62,106 @@
                 Console.Write("Guess a letter: ");
                 letterGuess = Console.ReadLine();
 
+                Console.WriteLine();
+
+                for (int i = 0; i < guessedLetters.Count; i++)
+                {
+                    if (letterGuess.ToUpper() == guessedLetters[i])
+                    {
+                        Console.Write("You can't guess a letter you have already guessed, please guess another letter: ");
+                        letterGuess = Console.ReadLine();
+                        i = -1;
+
+                        Console.WriteLine();
+                    }
+                }
+
                 while (letterGuess.Length != 1)
                 {
-                    Console.WriteLine();
-
                     Console.Write("That is not a single letter, please enter just one letter: ");
                     letterGuess = Console.ReadLine();
+
+                    Console.WriteLine();
+
+                    for (int i = 0; i < guessedLetters.Count; i++)
+                    {
+                        if (letterGuess.ToUpper() == guessedLetters[i])
+                        {
+                            Console.Write("You can't guess a letter you have already guessed, please guess another letter: ");
+                            letterGuess = Console.ReadLine();
+                            i = -1;
+
+                            Console.WriteLine();
+                        }
+                    }
                 }
-                
-                if (secretWord.Contains(letterGuess.ToUpper()))
+
+                letterGuess = letterGuess.ToUpper();
+
+                guessedLetters.Add(letterGuess);
+
+                if (secretWord.Contains(letterGuess))
                 {
                     Console.WriteLine("Correct guess!");
+
+                    while (unguessedLetters.Contains(letterGuess))
+                    {
+                        letterIndex = unguessedLetters.IndexOf(letterGuess);
+                        unguessedLetters = unguessedLetters.Remove(letterIndex, 1);
+                        unguessedLetters = unguessedLetters.Insert(letterIndex, "_");
+
+                        displayWord = displayWord.Remove(letterIndex, 1);
+                        displayWord = displayWord.Insert(letterIndex, letterGuess);
+                    }
                 }
-                else if (!secretWord.Contains(letterGuess.ToUpper()))
+                else if (!secretWord.Contains(letterGuess))
                 {
                     Console.WriteLine("Incorrect guess...");
                     incorrectGuesses += 1;
+                }
+
+                Thread.Sleep(500);
+
+                Console.Clear();
+
+                if (displayWord == secretWord)
+                {
+                    Console.WriteLine("Congratulations, you have guessed the word before you ran out of incorrect guesses!");
+                    done = true;
+
+                    Console.WriteLine();
+
+                    if (incorrectGuesses == 0)
+                    {
+                        DrawHangmanGuessZero();
+                    }
+                    else if (incorrectGuesses == 1)
+                    {
+                        DrawHangmanGuessOne();
+                    }
+                    else if (incorrectGuesses == 2)
+                    {
+                        DrawHangmanGuessTwo();
+                    }
+                    else if (incorrectGuesses == 3)
+                    {
+                        DrawHangmanGuessThree();
+                    }
+
+                    Console.WriteLine();
+
+                    Console.WriteLine(displayWord);
+                }
+                else if (incorrectGuesses == 3)
+                {
+                    Console.WriteLine("You ran out of incorrect guesses before guessing the word... The word was " + secretWord + ".");
+                    done = true;
+
+                    DrawHangmanGuessThree();
+
+                    Console.WriteLine();
+
+                    Console.WriteLine(displayWord);
                 }
             }
         }
@@ -99,7 +193,7 @@
             Console.WriteLine("  +---+");
             Console.WriteLine("  |   |");
             Console.WriteLine("  O   |");
-            Console.WriteLine(" /|\\  |");
+            Console.WriteLine("  |   |");
             Console.WriteLine("      |");
             Console.WriteLine("      |");
             Console.WriteLine("=========");
@@ -110,8 +204,19 @@
             Console.WriteLine("  +---+");
             Console.WriteLine("  |   |");
             Console.WriteLine("  O   |");
-            Console.WriteLine(" /|\\  |");
-            Console.WriteLine(" / \\  |");
+            Console.WriteLine(" /|   |");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+            Console.WriteLine("=========");
+        }
+
+        static void DrawHangmanGuessFour()
+        {
+            Console.WriteLine("  +---+");
+            Console.WriteLine("  |   |");
+            Console.WriteLine("  O   |");
+            Console.WriteLine(" /|   |");
+            Console.WriteLine("      |");
             Console.WriteLine("      |");
             Console.WriteLine("=========");
         }
